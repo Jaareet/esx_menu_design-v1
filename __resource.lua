023134a1ed378d06252dcb07931fd4f911b0bb8c
canariodev <80985233+canariodev@users.noreply.github.com>
2021-08-18 14:00:27 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+-- 
+
+description 'esx_menu_default Redesigned by canariodev#0001'
+
+files {
+	'**/**/**/*.*'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+client_scripts {
+	'client/main.lua'
+}
