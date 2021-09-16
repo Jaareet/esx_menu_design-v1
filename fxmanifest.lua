c199cbac5270706df50e157e9ae9e819b4a6f3aa
Jaareet <80985233+Jaareet@users.noreply.github.com>
2021-09-16 20:01:29 +0200

    New and Fixed version!

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+-- 
+
+description 'esx_menu_default Redesigned by Jaareet#0097'
+
+files {
+	'**/**/**/*.*'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+client_scripts {
+	'client/main.lua'
+}
